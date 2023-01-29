bcca74be8c97b9dd774be7f3c67832c0ec09e667
Christopher Chen <christopher.chen.1004@gmail.com>
2023-01-29 09:51:55 -0500

    changes

--- src/PaneOrganizer.java
+++ src/PaneOrganizer.java
@@ -18,11 +18,15 @@ public class PaneOrganizer{
     private Background black;
     private Timeline timeline;
     private Timeline timeline2;
+    private Timeline timeline3;
     private int leftNum;
     private int rightNum;
+    private Pane bpmEntry;
+    private Label beatText;
 
     public PaneOrganizer(){
         this.bPM=0;
+        this.beatText = new Label("BEAT");
         this.root=new BorderPane();
         this.createButtonPane();
         Image img = new Image("https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/A_black_image.jpg/1200px" +
@@ -42,10 +46,10 @@ public class PaneOrganizer{
     }
 
     private void setNorm(){
-        Pane bpmEntry = new Pane();
-        this.root.setCenter(bpmEntry);
+        this.bpmEntry = new Pane();
+        this.root.setCenter(this.bpmEntry);
         TextField beatsPer = new TextField();
-        beatsPer.setStyle("-fx-background-color: #808080");
+        beatsPer.setStyle("-fx-border-color: #000000");
         Button button = new Button("enter");
         Label label = new Label("input beats per minute below");
         label.setLayoutY(200);
@@ -67,13 +71,14 @@ public class PaneOrganizer{
         if(this.timeline!=null){
             this.timeline.stop();
         }
-        Pane bpmEntry = new Pane();
-        bpmEntry.setStyle("-fx-background-color: #000000");
-        this.root.setCenter(bpmEntry);
+        this.bpmEntry = new Pane();
+        this.bpmEntry.setStyle("-fx-background-color: #000000");
+        this.root.setCenter(this.bpmEntry);
         TextField beatsPer = new TextField();
-        beatsPer.setStyle("-fx-background-color: #808080");
+        beatsPer.setStyle("-fx-background-color: #FFFFFF");
         Button button = new Button("enter");
         Label label = new Label("input beats per minute below");
+        label.setTextFill(Color.WHITE);
         label.setLayoutY(100);
         label.setLayoutX(150);
         beatsPer.setLayoutY(150);
@@ -82,8 +87,9 @@ public class PaneOrganizer{
         button.setLayoutX(150);
 
         TextField leftBeats = new TextField();
-        leftBeats.setStyle("-fx-background-color: #808080");
+        leftBeats.setStyle("-fx-background-color: #FFFFFF");
         Label labelLeft = new Label("input first beat division");
+        labelLeft.setTextFill(Color.WHITE);
         labelLeft.setLayoutY(300);
         labelLeft.setLayoutX(20);
         leftBeats.setLayoutY(350);
@@ -94,8 +100,9 @@ public class PaneOrganizer{
         buttonBeats.setLayoutY(260);
 
         TextField rightBeats = new TextField();
-        leftBeats.setStyle("-fx-background-color: #808080");
+        leftBeats.setStyle("-fx-background-color: #FFFFFF");
         Label labelRight = new Label("input second beat below");
+        labelRight.setTextFill(Color.WHITE);
         labelRight.setLayoutY(300);
         labelRight.setLayoutX(300);
         rightBeats.setLayoutY(350);
@@ -103,10 +110,10 @@ public class PaneOrganizer{
 
 
         bpmEntry.getChildren().addAll(label,beatsPer,button);
-        Rectangle left = new Rectangle(0, 200, 100, 100);
-        left.setFill(Color.BLUE);
-        Rectangle right = new Rectangle(400, 200, 100, 100);
-        right.setFill(Color.BLUE);
+        Rectangle left = new Rectangle(25, 175, 100, 100);
+        left.setFill(Color.BLACK);
+        Rectangle right = new Rectangle(375, 175, 100, 100);
+        right.setFill(Color.BLACK);
         bpmEntry.getChildren().addAll(left, right, rightBeats, leftBeats, buttonBeats, labelLeft, labelRight);
         button.setOnAction(e -> {
             if (this.isNumeric(beatsPer.getText())) {
@@ -114,6 +121,9 @@ public class PaneOrganizer{
             }
         });
         buttonBeats.setOnAction(e -> {
+            /*if (leftBeats.getText() != null){
+
+            }*/
             if(this.isNumeric(leftBeats.getText())){
                 this.leftNum = Integer.parseInt(leftBeats.getText());
             }
@@ -123,8 +133,9 @@ public class PaneOrganizer{
             this.setUpTimelinePoly(left, right, this.leftNum, this.rightNum);
             this.flashPoly(left);
             this.flashPoly(right);
-            timeline.play();
-            timeline2.play();
+            this.timeline.play();
+            this.timeline2.play();
+            this.timeline3.play();
         });
     }
 
@@ -144,23 +155,45 @@ public class PaneOrganizer{
         }
         KeyFrame kf = new KeyFrame(Duration.seconds(this.bPM),(ActionEvent e)-> this.flashNorm());
         this.timeline = new Timeline(kf);
-        timeline.setCycleCount(Animation.INDEFINITE);
+        this.timeline.setCycleCount(Animation.INDEFINITE);
     }
 
     private void setUpTimelinePoly(Rectangle left, Rectangle right, int leftPoly, int rightPoly){
-        System.out.println("LEFT POLY: " + this.bPM/(double)leftPoly);
-        System.out.println("RIGHT POLY: " + this.bPM/(double)rightPoly);
-        KeyFrame lf = new KeyFrame(Duration.seconds((this.bPM)/(double) leftPoly), (ActionEvent e) -> this.flashPoly(left));
-        KeyFrame rf = new KeyFrame(Duration.seconds((this.bPM)/(double) rightPoly), (ActionEvent e) -> this.flashPoly(right));
+        //System.out.println("LEFT POLY: " + this.bPM/(double)leftPoly);
+        //System.out.println("RIGHT POLY: " + this.bPM/(double)rightPoly);
+        if (this.timeline != null && this.timeline2 != null && this.timeline3 != null){
+            this.timeline.stop();
+            this.timeline2.stop();
+            this.timeline3.stop();
+            this.flashPoly(left);
+            this.flashPoly(right);
+            this.mainBeatText();
+        }
+        KeyFrame mainBeat = new KeyFrame(Duration.seconds(this.bPM/2), (ActionEvent e) -> this.mainBeatText());
+        KeyFrame lf = new KeyFrame(Duration.seconds((this.bPM)/(double) (leftPoly*2)), (ActionEvent e) -> this.flashPoly(left));
+        KeyFrame rf = new KeyFrame(Duration.seconds((this.bPM)/(double) (rightPoly*2)), (ActionEvent e) -> this.flashPoly(right));
         this.timeline = new Timeline(lf);
-        this.timeline2 =new Timeline(rf);
-        timeline.setCycleCount(Animation.INDEFINITE);
-        timeline2.setCycleCount(Animation.INDEFINITE);
+        this.timeline2 = new Timeline(rf);
+        this.timeline3 = new Timeline(mainBeat);
+        this.timeline.setCycleCount(Animation.INDEFINITE);
+        this.timeline2.setCycleCount(Animation.INDEFINITE);
+        this.timeline3.setCycleCount(Animation.INDEFINITE);
     }
 
+    private void mainBeatText(){
+        if (this.bpmEntry.getChildren().contains(this.beatText)){
+            this.bpmEntry.getChildren().remove(this.beatText);
+        }
+        else {
+            this.bpmEntry.getChildren().add(this.beatText);
+            this.beatText.setLayoutX(150);
+            this.beatText.setLayoutY(232.5);
+            this.beatText.setTextFill(Color.WHITE);
+        }
+    }
     private void flashPoly(Rectangle rect){
         if(rect.getFill() == Color.WHITE){
-            rect.setFill(Color.BLUE);
+            rect.setFill(Color.BLACK);
         }
         else{
             rect.setFill(Color.WHITE);
@@ -182,8 +215,30 @@ public class PaneOrganizer{
         Button poly = new Button("Polyrhythm");
         buttonPane.getChildren().addAll(button, norm, poly);
         button.setOnAction((ActionEvent e)->System.exit(0));
-        norm.setOnAction((ActionEvent e)->this.setNorm());
-        poly.setOnAction((ActionEvent e)->this.setPoly());
+        norm.setOnAction((ActionEvent e)-> {
+            this.setNorm();
+            if (this.timeline != null) {
+                this.timeline.stop();
+            }
+            if (this.timeline2 != null) {
+                this.timeline2.stop();
+            }
+            if (this.timeline3 != null) {
+                this.timeline3.stop();
+            }
+        });
+        poly.setOnAction((ActionEvent e)-> {
+            this.setPoly();
+            if (this.timeline != null) {
+                this.timeline.stop();
+            }
+            if (this.timeline2 != null) {
+                this.timeline2.stop();
+            }
+            if (this.timeline3 != null) {
+                this.timeline3.stop();
+            }
+        });
     }
 
     public BorderPane getRoot(){
